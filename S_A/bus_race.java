e75f4b9c57c2c05cba71a5e573399fcfa0ac7a41
github oyg01285@naver.com <oyg01285@naver.com>
2023-02-04 04:52:01 +0900

    SA 완료. 기름이 10 이하 일시 차고지로 버스 이동하지만, 그 상태에서 버스 위치를 차고지로 또 입력하면 다른 이벤트가 나와야 하지만 너무 졸려서 차마 거기까진 못하겠다.

--- S_A/bus_race.java
+++ S_A/bus_race.java
@@ -6,26 +6,39 @@ public class bus_race extends bus {
 
     int now_passenger_num = 0;
 
-    int passengers_num = 0;
+    int inside_passenger = 0;
 
     String[] to_garage = {""};
 
+    int total_cost = 0;
+
     public void bus_name_num(int e) {
-        for(int i =0; i < e ; i++) {
-            this.bus_num = i+1;
-            System.out.println( bus_num+"번 버스 객체 만들어짐");
+        for (int i = 0; i < e; i++) {
+            this.bus_num = i + 1;
+            System.out.println(bus_num + "번 버스 객체 만들어짐");
         }
     }
 
+    public int bus_name_select(int a) {
+        bus_num = a;
+        System.out.println(bus_num + "번 버스 객체를 사용합니다.");
+        return bus_num;
+    }
+
+
     public String situation(String a) {
         this.situation = a;
-        if (situation.equals("차고지행")) {
-            System.out.println("주유량 =" + gas);
-            now_passenger_num = 0;
-        } else if(situation.equals("운행중")) {
-            System.out.println("운행중");
-        } else{
-            System.out.println("다시 입력하세요");
+        if (gas <= 10) {
+            situation = "차고지행";
+            System.out.println("기름이 부족하여 차고지를 떠날 수 없습니다.");
+        }else{
+            if (situation.equals("차고지행")) {
+                System.out.println("상태 = " + situation);
+                System.out.println("주유량 = " + gas);
+                now_passenger_num = 0;
+            } else if (situation.equals("운행중")) {
+                System.out.println("상태 = " + situation);
+            }
         }
         return situation;
     }
@@ -35,27 +48,44 @@ public class bus_race extends bus {
         if (situation.equals("운행중")) {
             if (now_passenger_num + b < this.max_passenger_num) {
                 now_passenger_num += b;
+                inside_passenger = max_passenger_num - now_passenger_num;
                 System.out.println("탑승 승객 수 =" + b);
-                System.out.println("잔여 승객 수 =" + (max_passenger_num - now_passenger_num));
+                System.out.println("잔여 승객 수 =" + (inside_passenger));
                 System.out.println("요금 확인 =" + b * this.cost);
-            } else if(now_passenger_num + b > this.max_passenger_num) {
+            } else if (now_passenger_num + b > this.max_passenger_num) {
                 System.out.println("최대 승객 수 초과");
             }
-        }else if(situation.equals("차고지행")){
+        } else if (situation.equals("차고지행")) {
             System.out.println("차량 운행 중이 아닙니다");
         }
         return now_passenger_num;
     }
 
-    public int gsa_Transition(int c){
-        if(situation.equals("차고지행")) {
+    public int passenger_quit_num(int b) {
+        if (situation.equals("운행중")) {
+            if (now_passenger_num - b > 0) {
+                now_passenger_num -= b;
+                inside_passenger = max_passenger_num - now_passenger_num;
+                System.out.println("하차 승객 수 =" + b);
+                System.out.println("잔여 승객 수 =" + (inside_passenger));
+            } else if (now_passenger_num - b < 0) {
+                System.out.println("내리는 사람이 타있는 사람보다 많은데 귀신이라도 탔습니까?");
+            }
+        } else if (situation.equals("차고지행")) {
+            System.out.println("차량 운행 중이 아닙니다");
+        }
+        return now_passenger_num;
+    }
+
+    public int gsa_Transition(int c) {
+        if (situation.equals("차고지행")) {
             this.gas += c;
-            System.out.println("주유량 = " + gas );
+            System.out.println("주유량 = " + gas);
             System.out.println("상태 =" + situation);
-        }else if(situation.equals("운행중")){
+        } else if (situation.equals("운행중")) {
             gas += c;
-            System.out.println("주유량 = " + gas );
-            if(gas < 10){
+            System.out.println("주유량 = " + gas);
+            if (gas < 10) {
                 System.out.println("주유 필요");
                 situation = "차고지행";
             }
@@ -63,10 +93,16 @@ public class bus_race extends bus {
         return gas;
     }
 
-    public int Speed (int d){
+    public int Speed(int d) {
         this.currentSpeed += d;
         return currentSpeed;
     }
 
+    public int getTotal_cost(int a) {
+        total_cost += cost;
+        return total_cost;
+    }
 }
+
+
 
--- S_A/execution.java
+++ S_A/execution.java
@@ -11,7 +11,12 @@ public class execution {
         System.out.println("버스의 수량을 입력하세요");
         int a = sc.nextInt();
         bus.bus_name_num(a);    // 버스 번호 생성
+        System.out.println();
 
+        System.out.println("정보를 입력할 버스 번호를 선택하세요");
+        int f = sc.nextInt();
+        bus.bus_name_select(f);
+        System.out.println();
 
 
         for(int b = 0; b <= 4;){
@@ -23,19 +28,51 @@ public class execution {
             b = sc.nextInt();
 
             if(b == 1) {
-                System.out.println("승객 수를 입력하세요");
-                int c = sc.nextInt();
-                bus.passenger_riding_num(c);
+                for(int g = 1 ;g < 2;){
+                    System.out.println("원하시는 승하차를 선택하세요");
+                    System.out.println("1.승차");
+                    System.out.println("2.하차");
+                    g = sc.nextInt();
+                        if(g == 1) {
+                            System.out.println("승객 수를 입력하세요");
+                            int c = sc.nextInt();
+                            bus.passenger_riding_num(c);
+                            System.out.println();
+                            break;
+                        }else if(g == 2) {
+                            System.out.println("승객 수를 입력하세요");
+                            int h = sc.nextInt();
+                            bus.passenger_quit_num(h);
+                            System.out.println();
+                            break;
+                            }
+                        }
             }else if(b == 2 ) {
-                System.out.println("버스 위치를 입력하세요");
-                String d = sc.next();
-                bus.situation(d);
+                for(int k = 1; k < 3;){
+                    System.out.println("버스 위치를 입력하세요");
+                    System.out.println("1. 운행중");
+                    System.out.println("2. 차고지행");
+                    k = sc.nextInt();
+                        if (k == 1 ) {
+                            bus.situation("운행중");
+                            System.out.println();
+                            break;
+                        }else if(k == 2 ) {
+                            bus.situation("차고지행");
+                            System.out.println();
+                            break;
+                    }
+                }
+
             }else if(b == 3) {
                 System.out.println("기름의 변화를 입력하세요");
                 int e = sc.nextInt();
                 bus.gsa_Transition(e);
+                System.out.println();
+
             }else if(b == 4){
                 System.out.println("종료 합니다.");
+                return;
             }
         }
     }
